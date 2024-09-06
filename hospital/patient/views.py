c9f67579425dd04c7ca0584a38189a5afb99d3e2
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-06 12:00:43 +0330

    Login refresh

--- hospital/patient/views.py
+++ hospital/patient/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
-# Create your views here.
+class Login(TokenObtainPairView):
+    pass
+
+class Refresh(TokenRefreshView):
+    pass
