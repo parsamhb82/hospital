02e520b039aa39db6062cddd2fa7bec3fbb6e88f
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-06 12:38:35 +0330

    fixes

--- hospital/reserve/models.py
+++ hospital/reserve/models.py
@@ -3,17 +3,19 @@ from patient.models import Patient
 from service.models import Service
 
 
+
 class Reserve(models.Model):
     patinet = models.ForeignKey(Patient, on_delete=models.CASCADE)
     date = models.DateField()
     time = models.TimeField()
     service = models.ForeignKey(Service, on_delete=models.CASCADE)
 
-    def patinet_name(self) -> str:
+    @property
+    def patinets_name(self) -> str:
         return self.patinet.fullname
-    
+    @property
     def service_price(self):
         return self.service.price
 
     def __str__(self) -> str:
-        return self.patinet
+        return self.patinet.fullname
