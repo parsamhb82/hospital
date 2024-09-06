f0364e5f0f01be7090626191293cb4130dd0affa
parsamhb82 <parsa1382mohebian@gmail.com>
2024-09-06 11:39:05 +0330

    patient and service added

--- hospital/patient/models.py
+++ hospital/patient/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Patient(models.Model):
+    fullname = models.CharField(max_length=100)
+
+    age = models.IntegerField()
+    gender = models.CharField(max_length=10)
+
+    def __str__(self) -> str:
+        return self.fullname
