5d1310c3b4831e74b1247d0677176cfcca599a07
Arshia <arshiamotaghian@yahoo.com>
2024-09-06 12:09:06 +0330

    Add new Serializer

--- hospital/service/views.py
+++ hospital/service/views.py
@@ -1,7 +1,8 @@
 from .serializer import *
 from .models import *
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView, ListAPIView
 from rest_framework.permissions import IsAuthenticated
+from .permissions import *
 
 
 class ServiceView(ListCreateAPIView):
@@ -20,3 +21,11 @@ class ServiceDetail(RetrieveUpdateDestroyAPIView):
 
     def get_queryset(self):
         return Service.objects.filter(user=self.request.user)
+
+
+class SuperUserView(ListAPIView):
+    queryset = Service.objects.all()
+    serializer_class = SuperUserReserveSerializer
+    permission_classes = [IsSuperUser]
+
+    
