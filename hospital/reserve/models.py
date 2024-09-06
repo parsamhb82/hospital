8314ee6bf3f5c4660322002739e14c0601fe11ab
Arshia <arshiamotaghian@yahoo.com>
2024-09-06 11:38:33 +0330

    Add reserve model

--- hospital/reserve/models.py
+++ hospital/reserve/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from patient.models import Patinet
+from service.models import Service
 
-# Create your models here.
+
+class Reserve(models.Model):
+    patinet = models.ForeignKey(Patinet, on_delete=models.CASCADE)
+    date = models.DateField()
+    time = models.TimeField()
+    service = models.ForeignKey(Service, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.patinet
